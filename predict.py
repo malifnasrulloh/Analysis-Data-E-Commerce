06e88f3d3ec0b03621855a31d02cd272a4791bfa
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2024-12-23 13:59:21 +0700

    add predict

--- predict.py
+++ predict.py
@@ -4,21 +4,30 @@ import pickle
 import tensorflow as tf
 import xgboost as xgb
 
+from tensorflow.keras import backend as K
+from tensorflow.keras.saving import register_keras_serializable
+
+@register_keras_serializable()
+def custom_mse(y_true, y_pred):
+    return K.mean(K.square(y_pred - y_true))
+
+
 @st.cache_resource
 def load_dl_model(path):
-    return tf.keras.models.load_model(path)
+    return tf.keras.models.load_model(path, custom_objects={"mse": custom_mse})
 
 @st.cache_resource
 def load_xgboost_model(path):
-    with open(path, "rb") as f:
-        model = pickle.load(f)
+    model = xgb.Booster()
+    model.load_model(path)
+    
     return model
 
 dl_price_model = load_dl_model("model/dl_model_price_prediction.h5")
 dl_delivery_model = load_dl_model("model/dl_model_delivery_prediction.h5")
 
-xgb_price_model = load_xgboost_model("model/xgboost_model_price_prediction.h5")
-xgb_delivery_model = load_xgboost_model("model/xgboost_model_delivery_prediction.h5")
+xgb_price_model = load_xgboost_model("model/xgboost_model_price_prediction.json")
+xgb_delivery_model = load_xgboost_model("model/xgboost_model_delivery_prediction.json")
 
 st.title("Prediksi E-Commerce")
 
@@ -30,7 +39,7 @@ prediction_type = st.sidebar.selectbox(
 if prediction_type == "Prediksi Harga Barang":
     st.header("Prediksi Harga Barang")
     
-    product_category_encoder = np.load("data/fix/state_encoder.npy",allow_pickle=True).tolist()
+    product_category_encoder = np.load("data/fix/product_category_name_encoder.npy",allow_pickle=True).tolist()
     product_category_decoder = {v:k for k, v in product_category_encoder.items()}
 
     
@@ -42,27 +51,30 @@ if prediction_type == "Prediksi Harga Barang":
     
     if st.button("Prediksi Harga"):
         input_data = np.array([[
-            product_category_name,
+            product_category_decoder[product_category_name],
             product_weight_g,
             product_length_cm,
             product_height_cm,
             product_width_cm
         ]])
+        
+        print(input_data)
+        print("masukkkkk")
 
         dl_price_pred = dl_price_model.predict(input_data)[0][0]
-
-        xgb_price_pred = xgb_price_model.predict(input_data)[0]
+        dmatrix_input = xgb.DMatrix(input_data)
+        xgb_price_pred = xgb_price_model.predict(dmatrix_input)[0]
 
         st.write("### Prediksi Harga Barang")
-        st.write(f"- **Deep Learning Model**: Rp {dl_price_pred:,.2f}")
-        st.write(f"- **XGBoost Model**: Rp {xgb_price_pred:,.2f}")
+        st.write(f"- **Deep Learning Model**: USD {dl_price_pred:,.2f}")
+        st.write(f"- **XGBoost Model**: USD {xgb_price_pred:,.2f}")
 
 elif prediction_type == "Prediksi Lama Pengiriman":
     st.header("Prediksi Lama Pengiriman")
     
     seller_customer_distance_km = st.number_input("Jarak Penjual-Pembeli (km)", min_value=0.0)
-    price = st.number_input("Harga Barang (Rp)", min_value=0.0)
-    freight_value = st.number_input("Nilai Ongkir (Rp)", min_value=0.0)
+    price = st.number_input("Harga Barang (USD)", min_value=0.0)
+    freight_value = st.number_input("Nilai Ongkir (USD)", min_value=0.0)
     product_weight_g = st.number_input("Berat Produk (gram)", min_value=0.0)
     product_length_cm = st.number_input("Panjang Produk (cm)", min_value=0.0)
     product_height_cm = st.number_input("Tinggi Produk (cm)", min_value=0.0)
@@ -80,10 +92,9 @@ elif prediction_type == "Prediksi Lama Pengiriman":
         ]])
 
         dl_delivery_pred = dl_delivery_model.predict(input_data)[0][0]
-
         dmatrix_input = xgb.DMatrix(input_data)
         xgb_delivery_pred = xgb_delivery_model.predict(dmatrix_input)[0]
 
         st.write("### Prediksi Lama Pengiriman")
-        st.write(f"- **Deep Learning Model**: {dl_delivery_pred:.2f} hari")
-        st.write(f"- **XGBoost Model**: {xgb_delivery_pred:.2f} hari")
+        st.write(f"- **Deep Learning Model**: {int(dl_delivery_pred)} hari")
+        st.write(f"- **XGBoost Model**: {int(xgb_delivery_pred)} hari")
